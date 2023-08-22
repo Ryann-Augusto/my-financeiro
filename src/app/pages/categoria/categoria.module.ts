bec7d6a66ffaf19b02392aac586e2add16e99748
Ryann Augusto <ryann.cruz97@gmail.com>
2023-08-22 15:45:11 -0300

    Criando tela de cadastro de sistemas

---
+++ src/app/pages/categoria/categoria.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { NavbarModule } from "src/app/components/navbar/navbar.module";
+import { SidebarModule } from "src/app/components/sidebar/sidebar.Module";
+import { CategoriaComponent } from "./categoria.component";
+import { CategoriaRoutingModule } from "./categoria-routing.module";
+
+@NgModule({
+    providers: [],
+    declarations: [CategoriaComponent],
+    imports: [
+        CommonModule,
+        CategoriaRoutingModule,
+        NavbarModule,
+        SidebarModule
+    ],
+})
+
+export class CategoriaModule{}
---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,48 @@
+import { Token } from '@angular/compiler';
+import { Component } from '@angular/core';
+
+import { FormBuilder, FormGroup, SelectControlValueAccessor, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoginService } from 'src/app/services/login.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+  constructor( public formBuilder: FormBuilder,
+    private router: Router,
+    private loginService: LoginService)
+  {
+
+  }
+  
+  loginForm: FormGroup;
+
+  ngOnInit(): void{
+    this.loginForm = this.formBuilder.group(
+      {
+        email :['',[Validators.required, Validators.email]],
+        senha :['',[Validators.required]]
+      }
+    )
+  }
+  get dadosForm()
+  {
+    return this, this.loginForm.controls
+  }
+
+  loginUser()
+  {
+    this.loginService.login(this.dadosForm["email"].value, this.dadosForm["senha"].value).subscribe(
+      token =>{
+        this.router.navigate(['/dashboard']);
+      },
+      err => {
+        alert('Ocorreu um erro')
+      }
+    )
+  }
+}
+
